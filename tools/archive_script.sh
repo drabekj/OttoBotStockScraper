e8e80b4b10e7f3597444d1ed11c6077b8d197605
Drabek Jan <drabek.honza@gmail.com>
2018-02-13 14:28:11 +0100

    + script for creating lambda .zip archive

---
+++ tools/archive_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PROJECT_FOLDER=~/Documents/Developer/Python/OttoBotDaemon
+ARCHIVE=app.zip
+
+VIRTUAL_ENV=/Users/drabekj/.virtualenvs/OttoBotDaemon
+DEPENDENCIES=${VIRTUAL_ENV}/lib/python3.6/site-packages
+
+# If the the archive file already exists cancel
+if [ -f ${PROJECT_FOLDER}/${ARCHIVE} ]; then
+    echo "Such file already exists!"
+    exit
+fi
+
+# zip dependencies
+cd ${DEPENDENCIES}
+zip -r9 ${PROJECT_FOLDER}/${ARCHIVE} *
+# zip function code
+cd ${PROJECT_FOLDER}
+zip -r9 ${ARCHIVE} lambda_function.py
+# zip config file
+cd ${PROJECT_FOLDER}
+zip -r9 ${ARCHIVE} rds_config.py
+
+echo "Archive created"
