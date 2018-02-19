abfb6f0868802c52bf7564969ff5547c2eb7194a
Drabek Jan <drabek.honza@gmail.com>
2018-02-19 17:46:38 +0100

    Update archive script to delete and/or upload archive to S3

--- tools/archive_script.sh
+++ tools/archive_script.sh
@@ -2,23 +2,34 @@
 
 PROJECT_FOLDER=~/Documents/Developer/Python/OttoBotDaemon
 ARCHIVE=app.zip
+AWS_S3_BUCKET=s3://ottobotstockscraperdaemonbucket/
 
-VIRTUAL_ENV=/Users/drabekj/.virtualenvs/OttoBotDaemonLambda
+VIRTUAL_ENV=/Users/drabekj/.virtualenvs/OttoBotDaemon
 DEPENDENCIES=${VIRTUAL_ENV}/lib/python3.6/site-packages
 
 # If the the archive file already exists cancel
 if [ -f ${PROJECT_FOLDER}/${ARCHIVE} ]; then
     echo "Such file already exists!"
-    exit
+
+    if [ "$1" == "-d" ]; then
+        echo "Deleting existing zip archive."
+    else
+        exit
+    fi
 fi
 
 # zip dependencies
+echo "Creating archive..."
 cd ${DEPENDENCIES}
-zip -r9 ${PROJECT_FOLDER}/${ARCHIVE} *
+zip -r9 ${PROJECT_FOLDER}/${ARCHIVE} * > /dev/null
 # zip function code
 cd ${PROJECT_FOLDER}
-zip -r9 ${ARCHIVE} lambda_function.py
-zip -r9 ${ARCHIVE} storage
-zip -r9 ${ARCHIVE} provider
-
+zip -r9 ${ARCHIVE} lambda_function.py > /dev/null
+zip -r9 ${ARCHIVE} storage > /dev/null
+zip -r9 ${ARCHIVE} provider > /dev/null
 echo "Archive created"
+
+if [ "$2" == "-u" ]; then
+    echo "Uploading archive to AWS S3"
+    aws s3 cp ${ARCHIVE} ${AWS_S3_BUCKET}
+fi
