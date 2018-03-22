e6e5d12c5ed2c4759f4ffc897398c57ef1198247
Drabek Jan <drabek.honza@gmail.com>
2018-03-22 10:31:14 +0100

    MOdify sample rds_config

--- storage/rds_config.py
+++ storage/rds_config.py
@@ -1,4 +1,4 @@
 #config file containing credentials for rds mysql instance
-db_username = "username"
-db_password = "password"
-db_name = "dbname"
+db_username = "myusername"
+db_password = "mypassword"
+db_name = "db_name"
